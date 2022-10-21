b0a3224cab31e8816e95b221fff1a81ae2cdeac9
victormedeiros1 <josevqczt@gmail.com>
2022-10-21 11:01:07 -0300

    feat: component Container

---
+++ src/components/Container/index.jsx
@@ -0,0 +1,7 @@
+import { ContainerStyles } from './styles';
+
+const Container = ({ children }) => {
+  return <ContainerStyles>{children}</ContainerStyles>;
+};
+
+export default Container;
---
+++ src/components/Container/styles.jsx
@@ -0,0 +1,10 @@
+import { styled } from '@stitches/react';
+
+export const ContainerStyles = styled('div', {
+  width: '100%',
+  minHeight: '100vh',
+  maxWidth: 1040,
+  textAlign: 'center',
+  padding: '$64 $20',
+  margin: '0 auto',
+});
