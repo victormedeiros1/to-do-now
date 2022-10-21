6a38a69dcb92e579e0fe8f2647c9f030822a4ca3
victormedeiros1 <josevqczt@gmail.com>
2022-10-21 11:10:47 -0300

    fix: component Line -> width using calc()

--- src/components/Divider/styles.jsx
+++ src/components/Divider/styles.jsx
@@ -1,8 +1,13 @@
 import { styled } from '@stitches/react';
 
 export const DividerStyles = styled('div', {
+  width: '100%',
   display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
   gap: 32,
 });
 
-export const Line = styled('hr', {});
+export const Line = styled('hr', {
+  width: 'calc(100% - 70%)',
+});
