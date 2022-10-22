2e695ae89d5d7d1fabad313a0c69849c8da86d97
victormedeiros1 <josevqczt@gmail.com>
2022-10-22 15:44:47 -0300

    feat: add BackgroundBars to main page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import Container from '../components/Container';
 import Divider from '../components/Divider';
 import Todos from '../components/Todos';
 import H1 from '../components/H1';
+import BackgroundBars from '../components/BackgroundBars';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <H1>TO-DO NOW</H1>
       <Divider />
       <Todos />
+      <BackgroundBars />
     </Container>
   );
 };
