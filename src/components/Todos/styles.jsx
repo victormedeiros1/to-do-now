28aa5e7251d1d4eccfce31928edfbf47b2efef1b
victormedeiros1 <josevqczt@gmail.com>
2022-10-22 20:51:53 -0300

    fix: bug container

--- src/components/Todos/styles.jsx
+++ src/components/Todos/styles.jsx
@@ -1,8 +1,11 @@
 import { styled } from '@stitches/react';
 
 export const TodosStyles = styled('div', {
+  maxHeight: 640,
+  overflowY: 'scroll',
   display: 'flex',
   flexDirection: 'column',
   gap: '0.5rem',
   margin: '$24 0',
+  padding: '$24',
 });
