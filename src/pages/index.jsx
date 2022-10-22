91ebf04e3c422e94f4b50503b88ccf04db037ec3
victormedeiros1 <josevqczt@gmail.com>
2022-10-22 15:06:09 -0300

    feat: components H1, Divider and Todos in main page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,16 @@
+import Container from '../components/Container';
+import Divider from '../components/Divider';
 import Todos from '../components/Todos';
+import H1 from '../components/H1';
+
 const Home = () => {
-  return <Todos />;
+  return (
+    <Container>
+      <H1>TO-DO NOW</H1>
+      <Divider />
+      <Todos />
+    </Container>
+  );
 };
 
 export default Home;
