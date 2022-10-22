2e37e4f89f06aa80f112c724bca8883fbd73d269
victormedeiros1 <josevqczt@gmail.com>
2022-10-22 15:44:02 -0300

    fix: CreateTodoButton border lightgray to gray

--- src/components/CreateTodo/styles.jsx
+++ src/components/CreateTodo/styles.jsx
@@ -1,7 +1,7 @@
 import { styled } from '@stitches/react';
 
 export const ButtonCreateTodo = styled('button', {
-  border: '1px dashed $lightgray',
+  border: '1px dashed $gray',
   background: 'none',
   transition: '0.1s',
   borderRadius: 4,
